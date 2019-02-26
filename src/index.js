d16646edede3631336bfa0b6a4795a24fc60f5df
Pjhl <seregal2009@yandex.ru>
2019-02-26 13:11:26 +0200

    Added eslint and index example

---
+++ src/index.js
@@ -0,0 +1,26 @@
+export async function sum (a, b) {
+  return Promise.resolve(a, b)
+}
+
+export default class Multiple {
+  static divider = '*'
+  static log = (...args) => {
+    console.log(...args)
+  }
+  constructor () {
+    this.a = 0
+    this.b = 0
+  }
+  setA (a) {
+    this.a = Number(a)
+  }
+  setB (b) {
+    this.b = Number(b)
+  }
+  render () {
+    const { a, b } = this
+    const divider = this.constructor.divider
+    const text = `${a} * ${b} = ${a*b}`
+    this.constructor.log(text)
+  }
+}
